5bd18ddcbfde9f7f7c2bafb14adabbccfed26c3a
stefanoruth <stefano6262@gmail.com>
2016-06-21 21:04:59 +0200

    Update so http:// is not needed

--- public/app.js
+++ public/app.js
@@ -7,6 +7,10 @@ $(document).ready(function(){
 	$('.controls form').submit(function(e){
 		e.preventDefault();
 		var url = $('.controls input[type=text]').val();
+		if (!url.match(/(http(s?):\/\/)/)) {
+			url = 'http://'+url;
+		}
+		console.log(url);
 		$('#viewIframe iframe').show().attr('src', url);
 	});
 });
