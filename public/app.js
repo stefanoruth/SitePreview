d8349d0c399f318ff3df9cce7a54f9a973621189
stefanoruth <stefano6262@gmail.com>
2016-06-21 23:04:51 +0200

    add option for linking to a site

--- public/app.js
+++ public/app.js
@@ -1,4 +1,17 @@
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
 $(document).ready(function(){
+	var frame = $('#viewIframe iframe');
+	var input = $('.controls input[type=text]');
+
 	$('.screens a').click(function(e){
 		e.preventDefault();
 		$('#viewIframe').removeClass().addClass($(this).data('screen'));
@@ -6,11 +19,18 @@ $(document).ready(function(){
 
 	$('.controls form').submit(function(e){
 		e.preventDefault();
-		var url = $('.controls input[type=text]').val();
+		var url = input.val();
 		if (!url.match(/(http(s?):\/\/)/)) {
 			url = 'http://'+url;
 		}
 		console.log(url);
-		$('#viewIframe iframe').show().attr('src', url);
+		frame.show().attr('src', url);
 	});
+
+	var query = getParameterByName('url');
+	if(query) {
+		console.log(query);
+		input.val(query);
+		frame.show().attr('src', query);
+	}
 });
